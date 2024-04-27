9c7fe32de5cc700d03f6725568bce28f17afecc0
unknown <lillianngoiri67@gmail.com>
2024-04-27 23:23:12 +0300

    Added a few comments to my code

--- django_project/views.py
+++ django_project/views.py
@@ -20,21 +20,21 @@ def index(request):
     return render(request, 'index.html', {'fact': fact})
 
 def facts(request): 
-    response = requests.get('https://uselessfacts.jsph.pl/random.json?language=en')
+    response = requests.get('https://uselessfacts.jsph.pl/random.json?language=en') #this will make GET request to the API endpoint to fetch a random fact
     data = response.json()
     fact = data['text']
-    return render(request, 'facts.html', {'fact': fact })
+    return render(request, 'facts.html', {'fact': fact }) # this will render the 'facts.html' template
 
 def dogs(request):
-    r3 = requests.get('https://dog.ceo/api/breeds/image/random')
+    r3 = requests.get('https://dog.ceo/api/breeds/image/random') #this will make GET request to the API endpoint to fetch a random fact
     res3 = r3.json()
     dog = res3['message']
-    return render(request, 'dogs.html', {'dog': dog })
+    return render(request, 'dogs.html', {'dog': dog }) # this will render the 'facts.html' template
 
 def students(request):
-    response = requests.get('https://freetestapi.com/api/v1/students')
+    response = requests.get('https://freetestapi.com/api/v1/students') #this will make GET request to the API endpoint to fetch a random fact
     data = response.json()
 
     # Selecting one random student
     random_student = choice(data)
-    return render(request, 'portal.html', {'student': random_student })
+    return render(request, 'portal.html', {'student': random_student }) # this will render the 'facts.html' template and pass the fact data to the template

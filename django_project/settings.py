550f1ac17c72c44dd17823382316ff6762bf5d46
unknown <lillianngoiri67@gmail.com>
2024-04-27 21:34:42 +0300

    Styled the navigation bar

--- django_project/settings.py
+++ django_project/settings.py
@@ -58,7 +58,7 @@ import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Include the path to your 'templates' directory
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
